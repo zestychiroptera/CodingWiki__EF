02f07462ef12f1eca8b9d68741184ca7cf1fdd1d
Zachary Waller <zachjives@gmail.com>
2024-01-22 18:13:08 -0500

    Create and update Get Action Method, Create and Update Category EF Core, Delete Category, Bulk Insert, Bulk Remove, and creation of Publisher and Author view(s).

---
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -0,0 +1,85 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using CodingWiki_Model.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingWiki_Web.Controllers
+{
+	public class BookController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+		{
+			List<Book> objList = _db.Books.ToList();
+            foreach(var obj in objList)
+            {
+                //obj.Publisher = _db.Publishers.Find(obj.Publisher_Id);
+                _db.Entry(obj).Reference(u=>u.Publisher).Load();
+            }
+			return View(objList);
+		}
+
+        public IActionResult Upsert(int? id)
+        {
+            BookVM obj = new();
+
+            obj.PublisherList = _db.Publishers.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Publisher_Id.ToString(),
+            });
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //edit
+            obj.Book = _db.Books.FirstOrDefault(u => u.IDBook == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Upsert(BookVM obj)
+        {
+            
+                if (obj.Book.IDBook == 0)
+                {
+                    //create
+                    await _db.Books.AddAsync(obj.Book);
+                }
+                else
+                {
+                    //update
+                    _db.Books.Update(obj.Book);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book obj = new();
+            obj = _db.Books.FirstOrDefault(u => u.IDBook == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Books.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
